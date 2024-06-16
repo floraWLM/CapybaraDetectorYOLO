95cb791df95943509335405fb0dd65ae7acb901e
floraWLM <lemengwangflora@163.com>
2024-06-16 16:26:24 -0400

    model trained with epochs = 1

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")
 
 # Train the model
-results = model.train(data="capy.yaml", epochs=3)
+results = model.train(data="capy.yaml", epochs=1,device="mps", project="runs/detect/")
+
+# Validate on training data
+model.val()
